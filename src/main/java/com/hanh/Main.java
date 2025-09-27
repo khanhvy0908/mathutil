6812c685ec79027994b3678c4074072d9480f2ff
hanhtran-se <hanhtransdr@gmail.com>
2025-09-27 15:28:42 +0700

    1st upload project message

---
+++ src/main/java/com/hanh/Main.java
@@ -0,0 +1,70 @@
+package com.hanh;
+
+import com.hanh.mathutil.com.MathUtil;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        testFactorialGivenRightArg0RunsWell();
+        //testFactorialGivenRightArg5RunsWell();
+        testFactorialGivenRightArg6RunsWell();
+    }
+    // test case #1: Check/Verify getFactorial() method with valid parameter, e.g. n = 0
+
+    // Steps/ procedure:
+    // 1. Given a valid n,  e.g. n = 0
+    // 2. call.invoke getFactorial(n=0)
+    // 3. execute
+    // EXPECTED RESULT:
+    // The expected value will return 1 as the result of 0!
+    // STATUS (PASSED | FAIL)
+    public static void testFactorialGivenRightArg0RunsWell(){
+        int n = 0;
+        long expectedValue = 1; // hope to see 0! = 1, kì vọng
+
+        // gọi hàm để xem actual long actualValue
+        long actualValue = MathUtil.getFactorial(n);
+        // test nè - so sánh actual vs expected
+        // dùng boolean hoặc in ta
+        System.out.println("0! expectedValue: " + expectedValue);
+        System.out.println("0! actualValue: " + actualValue);
+
+
+    }
+
+    // test case #2: Check/Verify getFactorial() method with valid parameter, e.g. n = 5
+    // Steps/ procedure:
+    // 1. Given a valid n,  e.g. n = 5
+    // 2. call.invoke getFactorial(n=5)
+    // 3. execute
+    // EXPECTED RESULT:
+    // The expected value will return 120 as the result of 5!
+    // STATUS (PASSED | FAIL)
+
+    public static void testFactorialGivenRightArg5RunsWell(){
+        int n = 5;
+        long expectedValue = 120; // hope to see 5! = 120, kì vọng
+
+        // gọi hàm để xem actual long actualValue
+        long actualValue = MathUtil.getFactorial(n);
+        // test nè - so sánh actual vs expected
+        // dùng boolean hoặc in ta
+        System.out.println("5! expectedValue: " + expectedValue);
+        System.out.println("5! actualValue: " + actualValue);
+    }
+
+    // test case #3: Check/Verify getFactorial() method with valid parameter, e.g. n = 6
+    // Steps/ procedure:
+    // 1. Given a valid n,  e.g. n = 6
+    // 2. call.invoke getFactorial(n=6)
+    // 3. execute
+    // EXPECTED RESULT:
+    // The expected value will return 720 as the result of 6!
+    // STATUS (PASSED | FAIL)
+    public static void testFactorialGivenRightArg6RunsWell(){
+        System.out.println("6! expected = 720 | actual = " + MathUtil.getFactorial(6));
+    }
+}
---
+++ src/main/java/com/hanh/mathutil/com/MathUtil.java
@@ -0,0 +1,31 @@
+package com.hanh.mathutil.com;
+
+public class MathUtil {
+
+    // class này chứa các hàm tiện ích dùng cho mọi nơi
+    // hàm tiện ích dùng cho mọi nơi , c nghĩa là nó không lưu lại kết quả xử lý của nó bên trong, vậy nó sẻ đc thiết kế là STATIC METHOD
+
+    // hàm tính n! = 1, 2, 3 ...n; trong đó n phải > 0
+    // quy ước 0! = 1, 1! = 1
+    // 20! vừa đủ kiểu long, 18 con số 0; 21! long chứa ko nổi
+    // ko có âm giai thừa
+    // bài này giai thừa chỉ tính từ 0 ... 20, ngoài vùng này gọi là ko hợp lệ n!
+    // 0... 20-> valid boundary validated boundary - vùng giá trị hợp ệ
+    // 0, 20 -> boudary value - biên giới cua tập giá trị
+    //          sích qua 1 síu là sang vùng invalid boundary
+    // 0 , xích nhẹ sang trái 1 đơn vị, ko ổn, ko tính đc, -1 sao tính
+    public static long getFactorial(int n){
+        if(n < 0 || n > 20){
+            // ném ngoại lệ, kèm câuchui73i, và dừng hàm ngay, ko có value nào dc tra về
+            throw new IllegalArgumentException("n must be between 0 and 20");
+        }
+        // biến trung gian để tính phép nhân
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;    // thuật toán heo đất
+        }
+
+        return result;
+
+    }
+}
