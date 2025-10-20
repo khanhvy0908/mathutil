2c3a9b44d8bab13a09a2a19a058987ff195d1d99
khanhvy0908 <khanhvy0946265560@gmail.com>
2025-10-20 11:34:47 +0700

    Update main source by using recursion to see green4ever

--- src/main/java/com/hanh/mathutil/com/MathUtil.java
+++ src/main/java/com/hanh/mathutil/com/MathUtil.java
@@ -14,17 +14,39 @@ public class MathUtil {
     // 0, 20 -> boudary value - biên giới cua tập giá trị
     //          sích qua 1 síu là sang vùng invalid boundary
     // 0 , xích nhẹ sang trái 1 đơn vị, ko ổn, ko tính đc, -1 sao tính
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20){
+//            // ném ngoại lệ, kèm câuchui73i, và dừng hàm ngay, ko có value nào dc tra về
+//            throw new IllegalArgumentException("n must be between 0 and 20");
+//        }
+//        // biến trung gian để tính phép nhân
+//        long result = 1;
+//        for (int i = 1; i <= n; i++) {
+//            result *= i;    // thuật toán heo đất
+//        }
+//
+//        return result;
+//    }
+
+    // viet ham theo phong cach de quy
+    // 5! = 1,2,3,4,5 = 4! .5 = 5 x 4!
+    // 4! = 4 x 3!
+    // 3! = 3 x 2!
+    // 2! = 2 x 1!
+    // 1! = 1
+    // ket luan: N! = N x (N - 1)! cong thuc de quy
+    // Recursion - goi lai chinh minh voi quy mo nho hon - bup be nga
     public static long getFactorial(int n){
         if(n < 0 || n > 20){
             // ném ngoại lệ, kèm câuchui73i, và dừng hàm ngay, ko có value nào dc tra về
             throw new IllegalArgumentException("n must be between 0 and 20");
         }
-        // biến trung gian để tính phép nhân
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;    // thuật toán heo đất
+        if (n == 1|| n ==0){
+            return 1;
         }
-
-        return result;
+        return n * getFactorial(n-1);
     }
+    // kiem thu lai code da toi uu - REGRESSION TESTING, TEST LAI THU DA TUNG TEST
+    
+
 }
